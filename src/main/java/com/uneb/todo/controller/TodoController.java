e6c0126cc35341a2d63bfec401b2941f53e34fce
Ernesto Junior <ernesto.sjunior@hotmail.com>
2025-12-16 20:16:32 -0300

    fix: add logging for todo creation and error handling in TodoController

--- src/main/java/com/uneb/todo/controller/TodoController.java
+++ src/main/java/com/uneb/todo/controller/TodoController.java
@@ -8,7 +8,6 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
